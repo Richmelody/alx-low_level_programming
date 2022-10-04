b2a6c87932a56a039388ae69d854c244dfdfb397
chigozie ikuru richards <chigs1million@gmail.com>
2022-10-03 22:11:01 -0700

    finished with 1 and 2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* A program that prints the name of an argument passed to it
+ * print a number followed by a new line
+ * argc, 
+ * argv, 
+ */
+int main(argc, char *argv[] __attribute__((__unused__)) )
+{	
+	printf("%d\n", argc);
+
+	return (0);
+}
